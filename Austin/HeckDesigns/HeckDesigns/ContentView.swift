ed683b68cc9c16a84c3e61015794149fdb72f7af
Moonkey <moonkey48@icloud.com>
2023-06-06 21:17:46 +0900

    [Feat] austin: tab bar 추가

--- Austin/HeckDesigns/HeckDesigns/ContentView.swift
+++ Austin/HeckDesigns/HeckDesigns/ContentView.swift
@@ -9,6 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        TabView {
+            Text("Heck")
+                .tabItem {
+                    Image(systemName: "exclamationmark.bubble.fill")
+                    Text("Heck")
+                }
+            Text("Nice")
+                .tabItem {
+                    Image(systemName: "checkmark.bubble.fill")
+                    Text("Nice")
+                }
+            Text("Issue")
+                .tabItem {
+                    Image(systemName: "questionmark.bubble.fill")
+                    Text("Issue")
+                }
+            
+        }
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
