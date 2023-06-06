e3d38a782d5629cdf4d57dd5d501ff79339507eb
Moonkey <moonkey48@icloud.com>
2023-06-06 22:03:31 +0900

    [Feat] austin: heck view sample 제작

--- Austin/HeckDesigns/HeckDesigns/View/HeckView.swift
+++ Austin/HeckDesigns/HeckDesigns/View/HeckView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct HeckView: View {
+    @State var listSize = 4
     var body: some View {
-        VStack {
-            Text("what the heck")
+        NavigationStack {
+            ScrollView {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Image("heck0")
+                            .resizable()
+                            .frame(width: 170, height: 170)
+                            .cornerRadius(10)
+                        Text("감성과 안전 사이")
+                            .font(Font.system(size: 20, weight: .bold))
+                    }
+                    VStack(alignment: .leading) {
+                        Image("heck1")
+                            .resizable()
+                            .frame(width: 170, height: 170)
+                            .cornerRadius(10)
+                        Text("아 뭘 사랑하냐고")
+                            .font(Font.system(size: 20, weight: .bold))
+                    }
+                }
+                HStack {
+                    VStack(alignment: .leading) {
+                        Image("heck2")
+                            .resizable()
+                            .frame(width: 170, height: 170)
+                            .cornerRadius(10)
+                        Text("따뜻한 콜드브루란..")
+                            .font(Font.system(size: 20, weight: .bold))
+                    }
+                    VStack(alignment: .leading) {
+                        Image("heck1")
+                            .resizable()
+                            .frame(width: 170, height: 170)
+                            .cornerRadius(10)
+                        Text("아 뭘 사랑하냐고")
+                            .font(Font.system(size: 20, weight: .bold))
+                    }
+                }
+                
+            }
+            .navigationTitle("Colors")
         }
     }
 }
